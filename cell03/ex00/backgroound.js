364ea467eb78d22197764c15237f7a64f4ba1396
Scelfo42 <rimscelfo.christian@gmail.com>
2023-03-22 18:26:48 +0100

    Day three

--- cell03/ex00/backgroound.js
+++ cell03/ex00/backgroound.js
@@ -1,8 +1,8 @@
 var button = document.getElementById("button");
 
 button.addEventListener("click", function() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
+    let letters = '0123456789ABCDEF';
+    let color = '#';
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
